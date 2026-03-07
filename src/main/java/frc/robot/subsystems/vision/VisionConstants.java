875c9f7046ca36af97313c5c9c56f65d0eb9f19e
Victor Tan <shenlain.nyc@gmail.com>
2026-03-07 18:00:56 -0500

    Camera names

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -17,8 +17,8 @@ public class VisionConstants {
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "camera_0";
-  public static String camera1Name = "camera_1";
+  public static String camera0Name = "limelight_0";
+  public static String camera1Name = "limelight_1";
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
