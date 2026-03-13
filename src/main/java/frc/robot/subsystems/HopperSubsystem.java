eb7eb7057745d3f4289c48e94ec7ce34e2b9e5e9
shkavalithomas-hub <shkavalithomas@gmail.com>
2026-03-13 17:13:06 -0400

    added some behavior changes and inverted motor

--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -1,18 +1,32 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.*;
 
 public class HopperSubsystem extends SubsystemBase {
 
     private boolean isRunning = false;
+    private final TalonFX HopperMotor;
+    private final CANrange rangeSensor;
+    
 
     public HopperSubsystem() {
         // Initialize motors, sensors, etc. here
-    }
+
+        HopperMotor = new TalonFX(34);
+        rangeSensor = new CANrange(35);
+        
+
+        HopperMotor.getConfigurator().apply(new TalonFXConfiguration());
+         } 
 
     private void setHopperSpeed(double speed) {
         // Code to set the speed of the hopper motor
+        HopperMotor.set(speed);
     }
 
     public void runHopper() {
