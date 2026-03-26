d4f4872441eb06dd309ac2315ea6a08f177c554a
shkavalithomas-hub <shkavalithomas@gmail.com>
2026-03-26 19:42:37 -0400

    Update Tower.java

--- src/main/java/frc/robot/subsystems/tower/Tower.java
+++ src/main/java/frc/robot/subsystems/tower/Tower.java
@@ -4,29 +4,12 @@
 
 package frc.robot.subsystems.tower;
 
-
-
-// | MEASUREMENTS | //
-import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.Volts;
-import edu.wpi.first.units.measure.AngularVelocity;
-
 // | configs + signals + hardware |//
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
 // |controls| //
 import com.ctre.phoenix6.controls.NeutralOut;
-
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -34,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 import frc.robot.subsystems.tower.TowerConstants;
