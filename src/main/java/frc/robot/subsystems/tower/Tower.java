b20b9104a11dc60d7e7ba1e9dc405bb6d13f5f15
shkavalithomas-hub <shkavalithomas@gmail.com>
2026-03-26 19:04:32 -0400

    revert, accidentaly published challenge to main

--- src/main/java/frc/robot/subsystems/tower/Tower.java
+++ src/main/java/frc/robot/subsystems/tower/Tower.java
@@ -4,64 +4,26 @@
 
 package frc.robot.subsystems.tower;
 
-
-
-// | MEASUREMENTS | //
-import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.Volts;
-import edu.wpi.first.units.measure.AngularVelocity;
-
-// | configs + signals + hardware |//
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-// |controls| //
 import com.ctre.phoenix6.controls.NeutralOut;
-
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import edu.wpi.first.networktables.BooleanPublisher;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-import frc.robot.subsystems.tower.TowerConstants;
+import frc.robot.subsystems.hopper.HopperConstants;
 
 public class Tower extends SubsystemBase {
-  private final TalonFX tower = new TalonFX(TowerConstants.TowerMotorId);
+  private final TalonFX tower = new TalonFX(33);
 
   private final VelocityVoltage towerRequest = new VelocityVoltage(0).withSlot(0).withEnableFOC(true);
   private final NeutralOut neutral = new NeutralOut();
 
-  private final DoublePublisher speedPublish;
-  private final BooleanPublisher atShootPublish;
-  private final BooleanPublisher atUnstuckPublish;
-
-  private static final double RPS_tolerance = 2.0;
-
   /** Creates a new Tower. */
   public Tower() {
     tower.getConfigurator().apply(TowerConstants.towerConfig);
-
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("Tower");
-    speedPublish    = table.getDoubleTopic("speed_rps").publish();
-    atShootPublish = table.getBooleanTopic("atShoot").publish();
-    atUnstuckPublish = table.getBooleanTopic("atUnstuck").publish();
   }
 
-  public void setTowerVelocity(double vel) {
+    public void setTowerVelocity(double vel) {
     tower.setControl(towerRequest.withVelocity(vel));
   }
 
@@ -70,34 +32,15 @@ public class Tower extends SubsystemBase {
   }
 
   public Command runTowerShoot() {
-    return this.runOnce(() -> this.setTowerVelocity(TowerConstants.TOWER_SHOOT_VELOCITY));
-  }
-
-  public double getRPS() {
-    return tower.getVelocity().getValueAsDouble();
-  }
-
-  public boolean atTargetSpeed(double tg_speed) {
-    return Math.abs(getRPS() - tg_speed) < RPS_tolerance;
-  }
-
-  public boolean atShootSpeed() {
-    return atTargetSpeed(TowerConstants.TOWER_SHOOT_VELOCITY);
-  }
-
-  public boolean atUnstuckSpeed() {
-    return atTargetSpeed(TowerConstants.TOWER_UNSTUCK_VELOCITY);
+    return this.runOnce(() -> this.setTowerVelocity(HopperConstants.HOPPER_SHOOT_VELOCITY));
   }
 
   public Command runTowerUnstuck() {
-    return this.runOnce(() -> this.setTowerVelocity(TowerConstants.TOWER_UNSTUCK_VELOCITY));
+    return this.runOnce(() -> this.setTowerVelocity(HopperConstants.HOPPER_UNSTUCK_VELOCITY));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    speedPublish.set(getRPS());
-    atShootPublish.set(atShootSpeed());
-    atUnstuckPublish.set(atUnstuckSpeed());
   }
 }
