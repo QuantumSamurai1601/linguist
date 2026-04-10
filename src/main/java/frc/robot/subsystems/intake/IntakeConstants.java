77664a97b10294546e46c9181192e6f24d8070f5
Victor Tan <shenlain.nyc@gmail.com>
2026-04-09 23:59:27 -0400

    Fudge current limits

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -10,8 +10,10 @@ public class IntakeConstants {
     intakeRollerConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     intakeRollerConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
-    intakeRollerConfig.CurrentLimits.SupplyCurrentLimit = 40;
+    intakeRollerConfig.CurrentLimits.SupplyCurrentLimit = 20;
+    intakeRollerConfig.CurrentLimits.StatorCurrentLimit = 60;
     intakeRollerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    intakeRollerConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
     intakeRollerConfig.Feedback.SensorToMechanismRatio = 2;
   }
@@ -21,8 +23,8 @@ public class IntakeConstants {
     intakeExtendConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     intakeExtendConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
-    intakeExtendConfig.CurrentLimits.SupplyCurrentLimit = 20;
-    intakeExtendConfig.CurrentLimits.StatorCurrentLimit = 60;
+    intakeExtendConfig.CurrentLimits.SupplyCurrentLimit = 40;
+    intakeExtendConfig.CurrentLimits.StatorCurrentLimit = 100;
     intakeExtendConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
     intakeExtendConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
@@ -34,29 +36,17 @@ public class IntakeConstants {
     intakeExtendConfig.Slot0.kS = 0.44;
     intakeExtendConfig.Slot0.kP = 350;
     intakeExtendConfig.Slot0.kD = 0;
-    intakeExtendConfig.MotionMagic.MotionMagicCruiseVelocity = 0.17;
-    intakeExtendConfig.MotionMagic.MotionMagicAcceleration = 1;
+    intakeExtendConfig.MotionMagic.MotionMagicCruiseVelocity = 0;
+    intakeExtendConfig.MotionMagic.MotionMagicAcceleration = 0;
 
     intakeExtendConfig.Feedback.SensorToMechanismRatio = 66;
-  }
-
-  public static final TalonFXConfiguration homingConfig = new TalonFXConfiguration();
-  static {
-    homingConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    homingConfig.CurrentLimits.StatorCurrentLimit = 50;
-    homingConfig.CurrentLimits.StatorCurrentLimitEnable = true;
-    homingConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
-    homingConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
   }
 
   public static final double INTAKING_VOLTS = 5.67;
   public static final double OUTAKING_VOLTS = -5.67;
   public static final double INTAKE_EXTEND_ASSIST_TIME_SEC = 0.1;
   public static final double INTAKE_STOW_POS = 0.23;
-  public static final double INTAKE_EXTEND_POS = 0.649;
-  public static final double INTAKE_COMPRESS_POS = 0.01;
-
-  public static final double INTAKE_HOMING_DUTY_CYCLE_OUT = 0.25;
-  public static final double INTAKE_HOMING_STATOR_CURRENT_THRES = 30;
-  public static final double INTAKE_HOMING_MAX_VELOCITY_THRES = 0.72;
+  public static final double INTAKE_EXTEND_POS = 0.645;
+  public static final double INTAKE_AGITATE_POS = 0.01;
+  public static final double INTAKE_AGITATE_MOVE_TIME_SEC = 0.67;
 }
