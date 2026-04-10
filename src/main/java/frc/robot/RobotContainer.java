32e4fd853160004f9a4fd287aad396ef1359dbe7
Victor Tan <shenlain.nyc@gmail.com>
2026-04-09 23:58:48 -0400

    Add dashboard outputs, moved Limelight IMU mode setting to mode triggers, condensed and refactored controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,27 +4,32 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.*;
-import static frc.robot.subsystems.vision.VisionConstants.*;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static frc.robot.subsystems.vision.VisionConstants.camera0Name;
+import static frc.robot.subsystems.vision.VisionConstants.camera1Name;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.FollowPathCommand;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+// import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
+// import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.hopper.Hopper;
@@ -43,13 +48,16 @@ public class RobotContainer {
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    // private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    // private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
+    //         .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
+    private final SlewRateLimiter sotmAccelerationLimX = new SlewRateLimiter(0.15);
+    private final SlewRateLimiter sotmAccelerationLimY = new SlewRateLimiter(0.15);
+    private final SlewRateLimiter sotmAccelerationLimRot = new SlewRateLimiter(0.02);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -69,21 +77,30 @@ public class RobotContainer {
             intake.extendIntake()
         ));
         NamedCommands.registerCommand("shootmfl", Commands.deadline(
-            new WaitCommand(4),
+            Commands.waitSeconds(4),
             hopper.runHopperShoot(),
-            tower.runTowerShoot()
+            tower.runTowerShoot(),
+            Commands.sequence(
+                Commands.waitSeconds(2),
+                intake.intakeAgitate()
+            )
         ).finallyDo(() -> {
             hopper.stopHopper();
             tower.stopTower();
         }));
         NamedCommands.registerCommand("shootmfr", Commands.deadline(
-            new WaitCommand(4),
+            Commands.waitSeconds(4),
             hopper.runHopperShoot(),
-            tower.runTowerShoot()
+            tower.runTowerShoot(),
+            Commands.sequence(
+                Commands.waitSeconds(2),
+                intake.intakeAgitate()
+            )
         ).finallyDo(() -> {
             hopper.stopHopper();
             tower.stopTower();
         }));
+
         autoChooser = AutoBuilder.buildAutoChooser("MFRLONG");
         SmartDashboard.putData("Auto Mode", autoChooser);
 
@@ -120,19 +137,6 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        // joystick.b().whileTrue(drivetrain.applyRequest(() ->
-        //     point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        // ));
-
-        // joystick.povUp().whileTrue(drivetrain.applyRequest(() ->
-        //     forwardStraight.withVelocityX(0.5).withVelocityY(0))
-        // );
-
-        // joystick.povDown().whileTrue(drivetrain.applyRequest(() ->
-        //     forwardStraight.withVelocityX(-0.5).withVelocityY(0))
-        // );
-
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
@@ -143,34 +147,95 @@ public class RobotContainer {
         // Reset the field-centric heading on left bumper press.
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+
         joystick.b().onTrue(Commands.either(
             intake.stowIntake(),
             intake.extendIntake(),
             () -> intake.isIntakeExtended
         ));
 
-        joystick.x().whileTrue(hopper.runHopperUnstuck());
-        joystick.x().onFalse(hopper.runOnce(() -> hopper.stopHopper()));
+        joystick.x().whileTrue(hopper.runHopperUnstuck())
+            .onFalse(hopper.runOnce(hopper::stopHopper));
 
-        joystick.back().onTrue(turret.runOnce(() -> turret.toggleTracking()));
+        joystick.y().whileTrue(intake.runOutake())
+            .onFalse(intake.runIntake());
 
-        joystick.start().onTrue(intake.runOnce(() -> intake.slowIntakeCompress()));
+        joystick.back().onTrue(turret.runOnce(turret::toggleTracking));
 
-        joystick.rightTrigger(0.5).onTrue(Commands.parallel(
-            hopper.runHopperShoot(),
-            tower.runTowerShoot()
-        ));
+        joystick.start().whileTrue(intake.intakeAgitate());
 
-        joystick.rightTrigger(0.5).onFalse(Commands.parallel(
-            hopper.runOnce(() -> hopper.stopHopper()),
-            tower.runOnce(() -> tower.stopTower())
-        ));
+        joystick.rightTrigger(0.5)
+            .and(turret::canShoot)
+            .whileTrue(Commands.parallel(
+                hopper.runHopperShoot(),
+                tower.runTowerShoot(),
+                turret.run(() -> turret.setHoodPosRot()),
+                drivetrain.applyRequest(() ->
+                    drive.withVelocityX(sotmAccelerationLimY.calculate(-joystick.getLeftY() * MaxSpeed * 0.5)) // Drive forward with negative Y (forward)
+                        .withVelocityY(sotmAccelerationLimX.calculate(-joystick.getLeftX() * MaxSpeed * 0.5)) // Drive left with negative X (left)
+                        .withRotationalRate(sotmAccelerationLimRot.calculate(-joystick.getRightX() * MaxAngularRate * 0.5)) // Drive counterclockwise with negative X (left)
+                )
+            ))
+            .onFalse(Commands.parallel(
+                hopper.runOnce(hopper::stopHopper),
+                tower.runOnce(tower::stopTower),
+                turret.runOnce(() -> turret.setHoodPosRot(0))
+            ));
 
-        joystick.povUp().onTrue(turret.runOnce(() -> turret.hoodInchUp()));
+        joystick.povUp().onTrue(turret.runOnce(turret::hoodInchUp));
+        joystick.povDown().onTrue(turret.runOnce(turret::hoodInchDown));
 
-        joystick.povDown().onTrue(turret.runOnce(() -> turret.hoodInchDown()));
+        // joystick.povUp().onTrue(turret.runOnce(() -> turret.shooterNudgeUp()));
+        // joystick.povDown().onTrue(turret.runOnce(() -> turret.shooterNudgeDown()));
+
+        // End-of-shift warning - FRC 6328
+        for (int i = 1; i <= 5; i++) {
+        double time = i;
+        Trigger shiftAboutToEnd =
+            new Trigger(() -> (HubShiftUtil.getShiftedShiftInfo().remainingTime() < time));
+        shiftAboutToEnd
+            .and(RobotModeTriggers.teleop())
+            .onTrue(
+                Commands.runEnd(
+                        () -> joystick.setRumble(RumbleType.kRightRumble, 1.0),
+                        () -> joystick.setRumble(RumbleType.kBothRumble, 0.0))
+                    .withTimeout(0.25));
+        }        
+        
+        // Reset hub shift timer when enabling - FRC 6328
+        RobotModeTriggers.teleop().onTrue(Commands.runOnce(HubShiftUtil::initialize));
+        RobotModeTriggers.autonomous().onTrue(Commands.runOnce(HubShiftUtil::initialize));
+        RobotModeTriggers.disabled().onTrue(Commands.runOnce(HubShiftUtil::initialize).ignoringDisable(true));
+
+        // Seed Limelight while disabled, fuse when enabled
+        RobotModeTriggers.disabled()
+            .whileTrue(Commands.runOnce(() -> LimelightHelpers.SetIMUMode("limelight-left", 1)));
+        RobotModeTriggers.disabled()
+            .whileTrue(Commands.runOnce(() -> LimelightHelpers.SetIMUMode("limelight-right", 1)));
+        RobotModeTriggers.autonomous()
+            .onTrue(Commands.runOnce(() -> LimelightHelpers.SetIMUMode("limelight-left", 4)));
+        RobotModeTriggers.autonomous()
+            .onTrue(Commands.runOnce(() -> LimelightHelpers.SetIMUMode("limelight-right", 4)));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    // FRC 6328
+    public void updateDashboardOutputs() {
+    // Publish match time
+    SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
+
+    // Update from HubShiftUtil
+    SmartDashboard.putString(
+        "Shifts/Remaining Shift Time",
+        String.format("%.1f", Math.max(HubShiftUtil.getShiftedShiftInfo().remainingTime(), 0.0)));
+    SmartDashboard.putBoolean("Shifts/Shift Active", HubShiftUtil.getShiftedShiftInfo().active());
+    SmartDashboard.putString(
+        "Shifts/Game State", HubShiftUtil.getShiftedShiftInfo().currentShift().toString());
+    SmartDashboard.putBoolean(
+        "Shifts/Active First?",
+        DriverStation.getAlliance().orElse(Alliance.Blue) == HubShiftUtil.getFirstActiveAlliance());        
     }
 
     public Command getAutonomousCommand() {
