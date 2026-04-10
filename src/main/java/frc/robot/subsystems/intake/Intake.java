516f45c4c178d1e280e09b4afaa7a75fffd416d7
Victor Tan <shenlain.nyc@gmail.com>
2026-04-09 23:59:01 -0400

    Add intake agitation

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,22 +4,16 @@
 
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 public class Intake extends SubsystemBase {
   private final TalonFX intakeRoller = new TalonFX(44);
@@ -27,10 +21,7 @@ public class Intake extends SubsystemBase {
 
   private final VoltageOut intakeRollerRequest = new VoltageOut(0).withEnableFOC(true);
   private final PositionVoltage intakeExtendRequest = new PositionVoltage(0).withSlot(0).withEnableFOC(true);
-  private final MotionMagicVoltage intakeCompressRequest = new MotionMagicVoltage(0).withSlot(0).withEnableFOC(true);
   private final NeutralOut neutral = new NeutralOut();
-
-  private final Debouncer debouncer = new Debouncer(0.1);
 
   public boolean isIntakeWheelOn = false;
   public boolean isIntakeExtended = false;
@@ -58,9 +49,22 @@ public class Intake extends SubsystemBase {
     }
   }
 
-  public void slowIntakeCompress() {
-    intakeExtend.setControl(intakeCompressRequest.withPosition(IntakeConstants.INTAKE_COMPRESS_POS));
-    isIntakeExtended = false;
+  public Command intakeAgitate() {
+    return new SequentialCommandGroup(
+      this.runOnce(() -> {
+        if (isIntakeExtended) {
+          this.setIntakePos(IntakeConstants.INTAKE_AGITATE_POS);
+          this.isIntakeExtended = false;
+        } else {
+          this.setIntakePos(IntakeConstants.INTAKE_EXTEND_POS);
+          this.isIntakeExtended = true;
+        }
+      }),
+      new WaitCommand(IntakeConstants.INTAKE_AGITATE_MOVE_TIME_SEC)
+    ).repeatedly().finallyDo(() -> {
+      this.setIntakePos(IntakeConstants.INTAKE_EXTEND_POS);
+      this.isIntakeExtended = true;
+    });
   }
 
   public void setIntakeNeutral() {
