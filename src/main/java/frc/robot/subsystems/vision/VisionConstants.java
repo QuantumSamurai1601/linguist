d1b71904608c3aba22087b8789cc0b3bd2cc200e
Victor Tan <shenlain.nyc@gmail.com>
2026-04-12 00:49:44 -0400

    Remove useless things

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -16,18 +16,12 @@ public class VisionConstants {
   // AprilTag layout
   public static AprilTagFieldLayout aprilTagLayout =
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
   public static double FIELD_WIDTH = aprilTagLayout.getFieldWidth();
 
   // Camera names, must match names configured on coprocessor
   public static String camera0Name = "limelight-left"; // 10.16.1.11
   public static String camera1Name = "limelight-right"; // 10.16.1.12
-
-  // Robot to camera transforms
-  // (Not used by Limelight, configure in web UI instead)
-  // public static Transform3d robotToCamera0 =
-  //     new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-  // public static Transform3d robotToCamera1 =
-  //     new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.2;
@@ -37,8 +31,8 @@ public class VisionConstants {
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 0.5; // Meters
-  public static double angularStdDevBaseline = 1; // Radians
+  public static double linearStdDevBaseline = 0.2; // Meters
+  public static double angularStdDevBaseline = 0.5; // Radians
 
   // Standard deviation multipliers for each camera
   // (Adjust to trust some cameras more than others)
@@ -52,31 +46,4 @@ public class VisionConstants {
   public static double linearStdDevMegatag2Factor = 1; // More stable than full 3D solve
   public static double angularStdDevMegatag2Factor =
       Double.POSITIVE_INFINITY; // No rotation data available
-
-    /** Represents the angle to a simple target, not used for pose estimation. */
-  public static record TargetObservation(Rotation2d tx, Rotation2d ty) {}
-
-  /** Represents a robot pose sample used for pose estimation. */
-  public static record PoseObservation(
-      double timestamp,
-      Pose3d pose,
-      double ambiguity,
-      int tagCount,
-      double averageTagDistance,
-      PoseObservationType type,
-      double averageTagArea) {}
-
-  public static enum PoseObservationType {
-    MEGATAG_1,
-    MEGATAG_2,
-    PHOTONVISION
-  }
-
-  public static class VisionInputs {
-    public boolean connected = false;
-    public TargetObservation latestTargetObservation =
-        new TargetObservation(Rotation2d.kZero, Rotation2d.kZero);
-    public PoseObservation[] poseObservations = new PoseObservation[0];
-    public int[] tagIds = new int[0];
-  }
 }
